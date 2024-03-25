e95ff5f5a7e00be6bbc6e60cc079fb865e63c943
Md Nahid Husen Nayan <nayanm@roehampton.ac.uk>
2024-03-25 22:24:02 +0000

    Updated index.js file to interact with all the  pages

--- Sprint_3/WebApp/src/index.js
+++ Sprint_3/WebApp/src/index.js
@@ -1,6 +1,11 @@
 // Import dependencies
 const express = require("express");
 const mysql = require("mysql2");
+const countryRoutes = require("./models/country"); // Import country routes
+const cityRoutes = require("./models/city"); // Import city routes
+const capitalRoutes = require("./models/capital"); // Import capital routes
+const languageRoutes = require("./models/language"); // Import language routes
+const populationRoutes = require("./models/populations"); // Import population report route
 
 
 // Create express instance
@@ -23,6 +28,9 @@ app.use(express.static('static'));
 // Use the routes by passing the app and db as arguments
 countryRoutes(app, db);
 cityRoutes(app, db);
+capitalRoutes(app, db);
+languageRoutes(app, db);
+populationRoutes(app, db);
 
 // Home Page route
 app.get("/", (req, res) => {
